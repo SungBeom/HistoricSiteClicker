2c56c5b55dd02663c61ebe1fcaa9526c68a5ac44
SimpleisNice <cgh3407@gmail.com>
2019-01-31 17:22:07 +0900

    0131_character_attack
    터치시 반응할 케릭터 게임상 반영 및 기본적인 에니메이션 추가
    - idle
    - attack
    - panel을 통한 공격

---
+++ HistoricSiteClicker/Assets/Scripts/ControlOfAttack.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class ControlOfAttack : MonoBehaviour {
+    //  기본 공격
+    //  todo : 기본 공격력 적용
+    //  todo : 기본 공격시 보스 체력 감소
+    public void NormalAttack()
+    {
+        GetComponent<Animator>().SetTrigger("NormalAttackTrigger");
+    }
+}
