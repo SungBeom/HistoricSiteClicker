28a3feb83f75311fc81c33c59b593afc5a512b98
SimpleisNice <cgh3407@gmail.com>
2019-02-06 17:29:07 +0900

    0206_add stage
    유적에 따른 스테이지 및 관련 내용 추가
    - 유적 파괴시 스테이지 변경 텍스트 추가
    - 유적 파괴시 스테이지에 따른 체력 변화 추가
    
    todo : 스테이지에 따른 유적 변화(0207)
    - 유적 공격시 에니메이션 추가
    - 유적 파괴시 유적 변경
    - 특정 단계의 유적 파괴시 유물 획득
    - 스테이지(ex. 사막)  유적 획득시 금전을 소모한 조합 및 버프 추가

---
+++ HistoricSiteClicker/Assets/Scripts/ControlOfMonster.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ControlOfMonster : MonoBehaviour {
+
+    public Animator Hero;
+    public Animator Monster;
+    public GameObject monsterHPImage;
+    public GameObject StageText;
+
+    //  공격 당함 
+    //  todo : 
+    public void BeAttacked()
+    {
+        //Hero.SetTrigger("NormalAttackTrigger");
+        Monster.SetTrigger("MonsterHitTrigger");
+
+        int monsterHP = Monster.GetInteger("MonsterHP");
+        int monsterTotalHP = Monster.GetInteger("MonsterTotalHP");
+        int heroDamage = Hero.GetInteger("NormalAttackDamage");
+
+
+        Monster.SetInteger("MonsterHP", monsterHP - heroDamage);
+
+        //  check monster die
+        if (Monster.GetInteger("MonsterHP") <= 0)
+        {
+            MonsterStageUp();
+            MonsterHPInit();
+            RelicsStage();
+            //Debug.Log("boss die and change");
+        }
+
+        //  change hp image
+        ChangeHPImage(monsterTotalHP, monsterHP);
+    }
+
+    void ChangeHPImage(float totalHP, float currentHP)
+    {
+        float currentImage = currentHP / totalHP;
+        monsterHPImage.GetComponent<Image>().fillAmount = (float)System.Math.Round(currentImage, 2);
+    }
+
+    //  몬스터 체력 초기화(stage에 따른 체력 변화)
+    void MonsterHPInit()
+    {
+        int stage = Monster.GetInteger("MonsterStage");
+        int HP = 1000 * stage;// * stage;
+        Monster.SetInteger("MonsterHP", HP);
+        Monster.SetInteger("MonsterTotalHP", HP);
+    }
+    //  유적 단계 변화
+    //  유적 파괴시 단계 증가
+    void MonsterStageUp()
+    {
+        int stage = Monster.GetInteger("MonsterStage");
+        Monster.SetInteger("MonsterStage", stage + 1);
+    }
+    //  몬스터 변경
+    //  스테이지 확인 후 변경
+    void MonsterChange()
+    {
+        //int stage = Monster.GetInteger("MonsterStage");
+        //Monster.SetInteger("MonsterStage", stage + 1);
+    }
+    void RelicsStage()
+    {
+        int stage = Monster.GetInteger("MonsterStage");
+        StageText.GetComponent<Text>().text = stage.ToString();
+    }
+}
